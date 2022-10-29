f959303dba193cfc2f3d11ce0d3f1fa894505303
songseul <song.codestates@gmail.com>
2022-10-29 19:05:53 +0900

    ⭐ feat :파일구조 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import IssueListPage from './pages/IssueListPage';
+import IssueDetailPage from './pages/IssueDetailPage';
+import Heading from './components/Heading';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Heading />
+      <Routes>
+        <Route path="/" element={<IssueListPage />} />
+        <Route path="/:nuber" element={<IssueDetailPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
