fbef9f60033e193b654e2ef6c683c52b87595991
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-10-30 22:16:37 +0900

    í¾¨ style : ErrorPage 스타일 수정 txt 글자를 조금 더 크게 하고, 텍스트를 가운데로 이동하였습니다.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,9 @@
 const ErrorPage = () => {
-  return <div>ERROR PAGE</div>;
+  return (
+    <div className="fixed flex items-center justify-center w-screen h-screen text-9xl">
+      ERROR PAGE
+    </div>
+  );
 };
 
 export default ErrorPage;
