b13655835174c48e33fc0e28b2166aaa3c5fd12e
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-10-31 00:48:00 +0900

    í·± build : api token 삭제 배포를 위하여 api 토큰을 삭제하였습니다.

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,3 @@
 import axios from 'axios';
 
-const token = process.env.REACT_APP_GITHUB_API_TOKEN;
-
 export const client = axios.create();
-client.defaults.headers.common.Authorization = `Bearer ${token}`;
