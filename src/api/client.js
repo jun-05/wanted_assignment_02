939ad50e784c9800ee116b946dbd002fb0e4cc66
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-10-30 16:41:27 +0900

    í»  fix : api에서 사용하는 env파일명 수정 env 파일 내용을 제대로 못 불러오는 이슈가 있어 수정하였습니다.

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const token = process.env.GITHUB_API_TOKEN;
+const token = process.env.REACT_APP_GITHUB_API_TOKEN;
+console.log(token);
 
 export const client = axios.create();
 client.defaults.headers.common.Authorization = `Bearer ${token}`;
