142337e17008280aa0354e13e59dcc1f1950e2d1
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-10-30 22:17:30 +0900

    ⭐ feat : DetailPage 기능 구현 - DetailContainer에서 현재 페이지의 쿼리 파라미터를 받아 데이터를 받아   사용하였습니다. - 마크다운을 표현하기 위해 tailwind/typography 와 marked 라이브러리를   설치하였습니다.

--- src/component/detail/DetailView.jsx
+++ src/component/detail/DetailView.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { marked } from 'marked';
 import tw from 'tailwind-styled-components';
-import { useParams } from 'react-router-dom';
-import { useIssueDispatch, useIssueState } from './../../context/IssueContext';
-import { getIssue } from '../../reducer/issue/issueAction';
+import { convertDate } from './../../utils/convertDate';
 
-const DetailView = () => {
-  const param = useParams();
-  const { issueNumber } = param;
-  const { issue } = useIssueState();
-  const dispatch = useIssueDispatch();
+const DetailView = ({ issueItem, Loading }) => {
+  const { created_at, body, user, title, comments, number } = issueItem;
+  const markdown = marked(body);
 
-  useEffect(() => {
-    getIssue(dispatch, issueNumber);
-  }, [dispatch, issueNumber]);
+  return (
+    <DetailWrapper>
+      <div className="flex flex-col  items-end text-2xl mt-10 w-[578px] pb-4 border-b-2">
+        <div>
+          #{number} <b>TITLE: </b>
+          {title}
+        </div>
 
-  console.log(issue);
+        <div className="font-sans text-sm">comments:{comments}</div>
+        <div className="mt-2 text-sm">{convertDate(created_at)}</div>
+        <div className="flex flex-col items-center">
+          <img
+            className="mt-10 h-10 w-10 rounded-full"
+            src={user.avatar_url}
+            alt={user.login}
+          ></img>
+          <div className=" text-xl">{user.login} </div>
+        </div>
+      </div>
 
-  return <DetailViewBlock>{issueNumber}</DetailViewBlock>;
+      <article className="prose prose-stone">
+        {<div dangerouslySetInnerHTML={{ __html: markdown }}></div>}
+      </article>
+    </DetailWrapper>
+  );
 };
-const DetailViewBlock = tw.div``;
+const DetailWrapper = tw.div`
+flex
+flex-col
+justify-center
+items-center
+`;
 
 export default DetailView;
--- src/container/detail/DetailContainer.jsx
+++ src/container/detail/DetailContainer.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Loading from '../../component/common/Loading';
+import { useIssueDispatch, useIssueState } from '../../context/IssueContext';
+import ErrorPage from '../../pages/ErrorPage';
+import { getIssue } from '../../reducer/issue/issueAction';
 import DetailView from './../../component/detail/DetailView';
 
 const DetailContainer = () => {
-  return <DetailView />;
+  const param = useParams();
+  const { issueNumber } = param;
+  const { issue } = useIssueState();
+  const dispatch = useIssueDispatch();
+  const { loading, data, error } = issue;
+
+  useEffect(() => {
+    getIssue(dispatch, issueNumber);
+  }, [dispatch, issueNumber]);
+
+  if (!data || loading) return <Loading />;
+  if (error || isNaN(issueNumber)) return <ErrorPage />;
+  return <DetailView issueItem={data} loading={loading} />;
 };
 
 export default DetailContainer;
